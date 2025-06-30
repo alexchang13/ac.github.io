a0fb54d3c84c0587c810f08a5f65f1c24ddf0e2f
alexchang <alex.chang@konnectnet.com>
2025-06-30 22:43:10 +1200

    Home Page and Projects Page Added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ACWebApp.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Projects()
         {
             return View();
         }
